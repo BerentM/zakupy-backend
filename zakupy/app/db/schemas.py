206107564ac078825752c7a369b397c52abc0c0e
BerentM <berentm@protonmail.com>
2023-02-22 19:42:28 +0100

    feat: return missing products count

--- zakupy/app/db/schemas.py
+++ zakupy/app/db/schemas.py
@@ -9,3 +9,12 @@ class ShoppingList(BaseModel):
     category: Optional[str]
     target_amount: Optional[int]
     current_amount: Optional[int] = 0
+
+
+class ShoppingListOut(ShoppingList):
+    product: str
+    source: str
+    category: str
+    target_amount: int
+    current_amount: int = 0
+    missing_amount: int = 0
--- zakupy/app/routes/shopping_list.py
+++ zakupy/app/routes/shopping_list.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 from fastapi import APIRouter, Depends
 
 import app.db.schemas as s
@@ -11,32 +13,46 @@ router = APIRouter(
 )
 
 
+async def calc_missing(data: ShoppingList) -> s.ShoppingListOut:
+    out = s.ShoppingListOut(**data.__dict__)
+    out.missing_amount = max(out.target_amount - out.current_amount, 0)
+    return out
+
+
 @router.get("/all")
-async def get_full_shopping_list(session=Depends(get_async_session)):
-    return await ShoppingListDAL.get_all(session=session)
+async def get_full_shopping_list(
+    session=Depends(get_async_session),
+) -> tuple[List[s.ShoppingListOut], int]:
+    data = await ShoppingListDAL.get_all(session=session)
+    return [await calc_missing(item) for item in data[0]], data[1]
 
 
 @router.get("/one")
-async def get_single_shopping_list_element(id: int, session=Depends(get_async_session)):
-    return await ShoppingListDAL.get_one(session=session, id=id)
+async def get_single_shopping_list_element(
+    id: int, session=Depends(get_async_session)
+) -> s.ShoppingListOut:
+    data = await ShoppingListDAL.get_one(session=session, id=id)
+    return await calc_missing(data)
 
 
 @router.post("/create_item")
 async def add_shopping_list_element(
     new_item: ShoppingList, session=Depends(get_async_session)
-):
+) -> ShoppingList:
     return await ShoppingListDAL.create(session=session, new_item=new_item)
 
 
 @router.delete("/delete_item")
-async def remove_shopping_list_element(id: int, session=Depends(get_async_session)):
+async def remove_shopping_list_element(
+    id: int, session=Depends(get_async_session)
+) -> ShoppingList:
     return await ShoppingListDAL.delete(session=session, delete_id=id)
 
 
 @router.patch("/update_item")
 async def update_shopping_list_element(
     id: int, new_data: s.ShoppingList, session=Depends(get_async_session)
-):
+) -> ShoppingList:
     return await ShoppingListDAL.update(
         session=session, update_id=id, new_item=new_data
     )
