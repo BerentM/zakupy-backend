c7f553b423bcc8ebc690a855a9cfc87e8b914e56
BerentM <berentm@protonmail.com>
2023-03-16 20:14:45 +0100

    feat: shopping list endpoint protection

--- zakupy/app/routes/shopping_list.py
+++ zakupy/app/routes/shopping_list.py
@@ -21,21 +21,23 @@ async def get_missing_product_list(
     source: Optional[str] = None,
     missing_percent: Optional[float] = 0,
     session=Depends(get_async_session),
-    user: User = Depends(current_active_verified_user), 
+    user: User = Depends(current_active_verified_user),
 ) -> s.ProductListAPI:
     data = await ProductDAL.get_all(
         session=session,
         missing_percent=missing_percent,
         source=source,
     )
-    return s.ProductListAPI(product_list=[await calc_missing(item) for item in data[0]], count=data[1])
+    return s.ProductListAPI(
+        product_list=[await calc_missing(item) for item in data[0]], count=data[1]
+    )
 
 
 @router.patch("/fill_up")
 async def fill_up_current_amount(
     ids: list[int],
     session: AsyncSession = Depends(get_async_session),
-    user: User = Depends(current_active_verified_user), 
+    user: User = Depends(current_active_verified_user),
 ):
     for id in ids:
         product = await ProductDAL.get_one(session, id)
